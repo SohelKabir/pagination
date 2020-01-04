13a218f6ee8337f1d8d67d114206f2a1553a15f7
Sohel Kabir <33397287+SohelKabir@users.noreply.github.com>
2020-01-05 00:05:44 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container mt-5'>
+        <h1 className='text-primary mb-3'>Pagination Challenge</h1>
+        <h2 className='text-secondary mb-3'>Timeline</h2>
+        <Posts posts={currentPosts} loading={loading} />
+
+        <ReactPaginate
+          pageCount={posts.length / 5}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={3}
+          initialPage={0}
+          onPageChange={v => paginate(v.selected + 1)}
+          containerClassName={'pagination'}
+          pageClassName={'page-link'}
+          nextClassName={'page-link'}
+          previousClassName={'page-link'}
+          activeClassName={'page-item active'}
+          disabledClassName={'page-item disabled'}
+          breakClassName={'page-link'}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
